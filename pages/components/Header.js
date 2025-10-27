68be10e19d9547393f8de8b6054e63fc3de4193f
mybella1810 <mybella1810@gmail.com>
2025-10-27 11:57:35 -0700

    Create Header.js

---
+++ pages/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "20px", textAlign: "center", background: "#f8f8f8" }}>
+      <h1>Pearl & Chrome Co.</h1>
+      <nav>
+        <a href="/">Home</a> | <a href="/shop">Shop</a> | <a href="/cart">Cart</a> | <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
