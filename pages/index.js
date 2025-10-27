7403077cf3100bddd9fb06f75f8ec983883813ee
mybella1810 <mybella1810@gmail.com>
2025-10-27 14:01:20 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductCard from "../components/ProductCard";
 import sampleData from "../sample-data.json";
-import styles from "../styles/styles.css";
 
 export default function Home() {
   return (
