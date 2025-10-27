4ac9838584817fda06f1fcc129223590d1f74d78
mybella1810 <mybella1810@gmail.com>
2025-10-27 11:58:09 -0700

    Create Footer.js

---
+++ pages/components/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "20px", textAlign: "center", background: "#f8f8f8", marginTop: "40px" }}>
+      <p>© {new Date().getFullYear()} Pearl & Chrome Co. | All rights reserved.</p>
+    </footer>
+  );
+}
